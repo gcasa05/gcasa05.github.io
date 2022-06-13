c8b1b76900077fdaa56bf7bdcf67b225b519baca
gcasa05 <giancarocasalegno@gmail.com>
2022-06-13 15:49:42 +0000

    saving walker

--- asd-projects/walker/index.js
+++ asd-projects/walker/index.js
@@ -10,20 +10,20 @@ function runProgram(){
   // Constant Variables
   var FRAME_RATE = 60;
   var FRAMES_PER_SECOND_INTERVAL = 1000 / FRAME_RATE;
-  var walkerPosX = 0;
+  var walkerPosX = 0;                     // setting the starter positions/speeds for the walker
   var walkerPosY = 0;
   var walkerSpeedX = 0;
   var walkerSpeedY = 0;
   $(document).on('keydown', handleKeyDown) //registers key down press
   // Game Item Objects
 var KEY = {
-  "left": 37, "right": 39, "up": 38, "down": 40,"none": 0,
+  "left": 37, "right": 39, "up": 38, "down": 40,"none": 0,     //is what registers which key is being pressed and allows us to move the walker accordingly
 }
 
   // one-time setup
   var interval = setInterval(newFrame, FRAMES_PER_SECOND_INTERVAL);   // execute newFrame every 0.0166 seconds (60 Frames per second)
-  $(document).on('keydown', handleKeyDown);  
-  $(document).on('keyup', handleKeyUp);  
+  $(document).on('keydown', handleKeyDown);  //updates the walker evertime a key is pressed
+  $(document).on('keyup', handleKeyUp);      //or released
                                                                         // change 'eventType' to the type of event you want to handle
 
   ////////////////////////////////////////////////////////////////////////////////
@@ -43,7 +43,7 @@ var KEY = {
   /* 
   Called in response to events.
   */
-  function handleKeyDown(event) {
+  function handleKeyDown(event) {                 //registers any key being pressed and alows us to move the walker based on it 
     if (event.which === KEY.down) {
       walkerSpeedY = 5;
   }else if  (event.which === KEY.up) {
@@ -51,7 +51,7 @@ var KEY = {
   }else if  (event.which === KEY.left) {
     walkerSpeedX = -5;
   }else if  (event.which === KEY.right) {
-    walkerSpeedX = 5;             //these print to the console what key was pressed
+    walkerSpeedX = 5;
 }}
 
   ////////////////////////////////////////////////////////////////////////////////
@@ -66,15 +66,15 @@ var KEY = {
     // turn off event handlers
     $(document).off();
   }
-  function repositionGameItem(){
+  function repositionGameItem(){      //moves the walker
 walkerPosX += walkerSpeedX;
 walkerPosY += walkerSpeedY;
   }
-  function redrawGameItem(){
+  function redrawGameItem(){          //updates the walker on the board
     $("#walker").css("left", walkerPosX); 
     $("#walker").css("top", walkerPosY);   
   }
-  function handleKeyUp(event){
+  function handleKeyUp(event){          //MAKES WALKER STOP MOVING using the key being pressed and released as a "trigger"
     if(event.which === KEY.up){
       walkerSpeedY = 0;
     }else if(event.which === KEY.down){
